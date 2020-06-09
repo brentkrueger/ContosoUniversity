ee116b3ceb21badc828e8a37d01359dc8610d356
Brent <brent.krueger@promega.com>
2020-06-09 16:21:57 -0500

    Step 5 of 8 completed

--- ContosoUniversity/Models/StudentVM.cs
+++ ContosoUniversity/Models/StudentVM.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class StudentVM
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
     }
